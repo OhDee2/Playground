4b582abeb7e2904432ec618f114b0e6ce01e19dc
James Farrell <jamesgjfarrell@gmail.com>
2018-04-04 20:24:23 +0100

    Singleton - not thread safe only

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    // this implementation is not thread safe
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+    }
+}
